4a1dd6af55b0eb67776cad0f4904db1519a95a64
vaibhav-holcim <vaibhav.veta@holcim.com>
2023-10-26 23:43:38 +0530

    as

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.vaibhav.devsecops',
   appName: 'DevSecOps',
   webDir: 'www',
   server: {
