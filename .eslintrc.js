005befe3783e99bc4bbffc167b62cf3531335f83
vaibhav-holcim <vaibhav.veta@holcim.com>
2023-10-27 15:13:12 +0530

    sasa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,33 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      jest: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: [
-      'react-app',
-      'airbnb',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['react', 'import', 'jsx-a11y'],
-    rules: {
-      'react/jsx-filename-extension': [
-        'error',
+    "overrides": [
         {
-          extensions: ['.tsx'],
-        },
-      ],
-      'import/prefer-default-export': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-member-accessibility': 'off'
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    settings: {
-      'import/parsers': {
-        '@typescript-eslint/parser': ['.ts', '.tsx'],
-      },
-      'import/resolver': {
-        typescript: {},
-      },
-    },
-  };
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
